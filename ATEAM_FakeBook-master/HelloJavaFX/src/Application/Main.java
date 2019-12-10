934826db1f34cb9fda4bcc975146ad4227941830
Johnsun <lsun89@wisc.edu>
2019-12-09 18:04:13 -0600

    OHOHOHOH

--- ATEAM_FakeBook-master/HelloJavaFX/src/Application/Main.java
+++ ATEAM_FakeBook-master/HelloJavaFX/src/Application/Main.java
@@ -1,6 +1,7 @@
 package Application;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.LinkedList;
 import java.util.List;
 import javax.security.auth.callback.ConfirmationCallback;
@@ -9,6 +10,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
@@ -22,12 +24,16 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -63,59 +69,94 @@ public class Main extends Application {
     clearButton.setPrefWidth(120);
     clearButton.setTranslateY(20);
 
+
     Button showAllNetWorkButton = new Button("ShowAllNetwork");
     showAllNetWorkButton.setPrefWidth(120);
     showAllNetWorkButton.setTranslateY(30);
+    
+    Button centeruserButton = new Button("Set CenterUser");
+    centeruserButton.setPrefWidth(120);
+    centeruserButton.setTranslateY(40);
+    centeruserButton.setOnAction(EventHandler -> centerUserWindow());
 
     VBox rightVBox = new VBox();
 
-    rightVBox.setPadding(new Insets(100, 20, 20, 20));
+    rightVBox.setPadding(new Insets(50, 20, 20, 20));
     rightVBox.getChildren().addAll(addButton, loadButton, removeButton, clearButton,
-        showAllNetWorkButton);
+        showAllNetWorkButton, centeruserButton);
 
     root.setRight(rightVBox);
 
+    // // Canvas
+    // Canvas canvas = new Canvas(200, 200);
+    // GraphicsContext gc = canvas.getGraphicsContext2D();
+    // // Write some text
+    // // Text is filled with the fill color
+    // gc.setFill(Color.GREEN);
+    // gc.setFont(new Font(30));
+    //
+    // // Draw a line
+    // // Lines use the stroke color
+    // gc.setStroke(Color.BLUE);
+    // gc.setLineWidth(2);
+    // gc.strokeLine(40, 100, 250, 50);
+    // // Draw a few circles
+    // gc.setFill(Color.BLACK);
+    // // The circles draw from the top left, so to center them, subtract the radius from each
+    // // coordinate
+    // gc.fillOval(40 - 15, 100 - 15, 30, 30);
+    // gc.setFill(Color.RED);
+    // gc.fillOval(250 - 15, 50 - 15, 30, 30);
 
-    // Canvas
-    Canvas canvas = new Canvas(200, 200);
-    GraphicsContext gc = canvas.getGraphicsContext2D();
-    // Write some text
-    // Text is filled with the fill color
-    gc.setFill(Color.GREEN);
-    gc.setFont(new Font(30));
-    gc.fillText("Hello World!", 70, 170);
-    // Draw a line
-    // Lines use the stroke color
-    gc.setStroke(Color.BLUE);
-    gc.setLineWidth(2);
-    gc.strokeLine(40, 100, 250, 50);
-    // Draw a few circles
-    gc.setFill(Color.BLACK);
-    // The circles draw from the top left, so to center them, subtract the radius from each
-    // coordinate
-    gc.fillOval(40 - 15, 100 - 15, 30, 30);
-    gc.setFill(Color.RED);
-    gc.fillOval(250 - 15, 50 - 15, 30, 30);
+    FileInputStream imageFile = new FileInputStream("./Network.jpg");
+    Image image = new Image(imageFile);
+    ImageView view = new ImageView(image);
+    view.setFitWidth(450);
+    view.setFitHeight(400);
+    view.setPreserveRatio(true);
+    view.setTranslateY(30);
+
 
     VBox centerVBox = new VBox();
-    
+
     Label l1 = new Label("Your Social Network");
     l1.setFont(new Font("MV Boli", 100));
     l1.setTranslateX(40);
     l1.setTranslateY(20);
-
-    centerVBox.getChildren().addAll(l1, canvas);
+    
+    centerVBox.getChildren().add(view);
+    
+    root.setTop(l1);
     root.setCenter(centerVBox);
 
     Scene mainScene = new Scene(root, 600, 400);
-    mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+    clearButton.setOnAction(EventHandler -> {
+      try {
+      centerVBox.getChildren().removeAll(centerVBox.getChildren().remove(centerVBox.getChildren().size()-1));
+      root.setCenter(centerVBox);
+      } catch (Exception e) {}
+    });
+ 
+    showAllNetWorkButton.setOnAction(e -> {
+      try {
+      centerVBox.getChildren().removeAll(view);
+      if(centerVBox.getChildren().size() > 0) centerVBox.getChildren().removeAll(centerVBox.getChildren().remove(centerVBox.getChildren().size()-1));
+      showfile1(centerVBox);
+      root.setCenter(centerVBox);
+      } catch (Exception m) {}
+    });
     
+
+    mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
     // Add the stuff and set the primary stage
     primaryStage.setTitle(APP_TITLE);
     primaryStage.setScene(mainScene);
     primaryStage.show();
 
   }
+
 
   // Start a new window after clicking the add button
   public void addWindow() {
@@ -133,7 +174,10 @@ public class Main extends Application {
     hbox1.getChildren().addAll(new Label("User: "), userField, confirmbutton1);
     confirmbutton1.setOnAction(EventHandler -> {
       try {
-        addSuccess();
+        if (userField.getText().isEmpty())
+          addFailure();
+        else
+          addSuccess();
       } catch (Exception e) {
         addFailure();
       }
@@ -148,8 +192,10 @@ public class Main extends Application {
     confirmbutton1.setTranslateX(37);
     confirmbutton2.setOnAction(EventHandler -> {
       try {
-
-        addSuccess();
+        if (relation1.getText().isEmpty() || relation2.getText().isEmpty())
+          addFailure();
+        else
+          addSuccess();
       } catch (Exception e) {
         addFailure();
       }
@@ -206,17 +252,22 @@ public class Main extends Application {
     t1.setTranslateX(-150);
     t1.setTranslateY(-5);
     hbox.getChildren().add(t1);
+    
 
     // create upload button and put it in the same hbox with textField
     Button button = new Button("UPLOAD");
     button.setTranslateX(-150);
     button.setTranslateY(-5);
+    
     hbox.getChildren().add(button);
+    
     button.setOnAction(EventHandler -> {
       try {
         File file = new File(t1.getText());
-        if(!file.exists()) loadFailure();
-        else loadSuccess();
+        if (!file.exists())
+          loadFailure();
+        else
+          loadSuccess();
       } catch (Exception e) {
         loadFailure();
       }
@@ -260,13 +311,14 @@ public class Main extends Application {
     HBox hbox1 = new HBox();
     TextField userField = new TextField();
     userField.setTranslateX(37);
-    
+
     Button confirmbutton1 = new Button("Confirm");
     confirmbutton1.setTranslateX(37);
     hbox1.getChildren().addAll(new Label("User: "), userField, confirmbutton1);
     confirmbutton1.setOnAction(EventHandler -> {
       try {
-        removeSuccess();
+        if (userField.getText().isEmpty()) removeFailure();
+        else removeSuccess();
       } catch (Exception e) {
         removeFailure();
       }
@@ -281,8 +333,8 @@ public class Main extends Application {
     hbox2.getChildren().addAll(new Label("Friendship: "), relation1, relation2, confirmbutton2);
     confirmbutton2.setOnAction(EventHandler -> {
       try {
-
-        removeSuccess();
+        if(relation1.getText().isEmpty() || relation2.getText().isEmpty()) removeFailure();
+        else removeSuccess();
       } catch (Exception e) {
         removeFailure();
       }
@@ -379,15 +431,15 @@ public class Main extends Application {
 
 
   }
-  
+
   public void removeSuccess() throws Exception {
-    
+
     Stage stage = new Stage();
     // save args example
     args = this.getParameters().getRaw();
 
     Label l1 = new Label("Remove success!");
-    
+
     // l1.Font = new Font(l1.Font.Name, 12, FontStyle.Bold | FontStyle.Underline);
     l1.setFont(new Font("Ariel", 20));
     l1.setTranslateX(66);
@@ -412,17 +464,17 @@ public class Main extends Application {
 
     stage.setScene(loadScene);
     stage.showAndWait();
-    
+
   }
 
   public void addFailure() {
-    
+
     Stage stage = new Stage();
     // save args example
     args = this.getParameters().getRaw();
 
     Label l1 = new Label("Add Fails!");
-    
+
     // l1.Font = new Font(l1.Font.Name, 12, FontStyle.Bold | FontStyle.Underline);
     l1.setFont(new Font("Ariel", 20));
     l1.setTranslateX(66);
@@ -447,17 +499,17 @@ public class Main extends Application {
 
     stage.setScene(loadScene);
     stage.showAndWait();
-    
+
   }
-  
+
   public void loadFailure() {
-    
+
     Stage stage = new Stage();
     // save args example
     args = this.getParameters().getRaw();
 
     Label l1 = new Label("Load Fails!");
-    
+
     // l1.Font = new Font(l1.Font.Name, 12, FontStyle.Bold | FontStyle.Underline);
     l1.setFont(new Font("Ariel", 20));
     l1.setTranslateX(66);
@@ -482,18 +534,18 @@ public class Main extends Application {
 
     stage.setScene(loadScene);
     stage.showAndWait();
-    
-    
+
+
   }
-  
+
   public void removeFailure() {
-    
+
     Stage stage = new Stage();
     // save args example
     args = this.getParameters().getRaw();
 
     Label l1 = new Label("Remove Fails!");
-    
+
     // l1.Font = new Font(l1.Font.Name, 12, FontStyle.Bold | FontStyle.Underline);
     l1.setFont(new Font("Ariel", 20));
     l1.setTranslateX(66);
@@ -518,9 +570,84 @@ public class Main extends Application {
 
     stage.setScene(loadScene);
     stage.showAndWait();
-    
-  } 
 
+  }
+  
+  public void showfile1(VBox center) {
+    
+    int WINDOW_WIDTH = 300;
+    int WINDOW_HEIGHT = 200;
+
+    Canvas canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+   
+    gc.setStroke(Color.BLUE);
+    gc.setLineWidth(1);
+    gc.strokeLine(48,68,138,68);
+    gc.strokeLine(48,68,88,123);
+    gc.strokeLine(88,123,138,68);
+    
+    gc.drawImage(createCircledNumber("mark"),30,50);
+   
+    gc.drawImage(createCircledNumber("Sid"), 120, 50);
+          
+    gc.drawImage(createCircledNumber("Sri"), 70, 105);
+    
+    center.getChildren().addAll(canvas);
+    
+   
+  }
+  
+  private WritableImage createCircledNumber(String name) {
+    StackPane sPane = new StackPane();
+    sPane.setPrefSize(36,36);        
+
+    Circle c = new Circle(36/2.0);
+    c.setStroke(Color.BLACK);
+    c.setFill(Color.WHITE);
+    c.setStrokeWidth(1);
+    sPane.getChildren().add(c);
+
+    Text txtNum = new Text(name + "");
+    sPane.getChildren().add(txtNum);
+    SnapshotParameters parameters = new SnapshotParameters();
+    parameters.setFill(Color.TRANSPARENT);
+    return sPane.snapshot(parameters, null);
+}
+  
+  // New window for setting center user
+  public void centerUserWindow() {
+    
+    Stage stage = new Stage();
+    
+    int WINDOW_WIDTH = 300;
+    int WINDOW_HEIGHT = 200;
+
+    Canvas canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+   
+    gc.setStroke(Color.BLUE);
+    gc.setLineWidth(1);
+    gc.strokeLine(48,68,138,68);
+    gc.strokeLine(48,68,88,123);
+    
+    gc.drawImage(createCircledNumber("mark"),30,50);
+    gc.drawImage(createCircledNumber("Sid"), 120, 50);
+    gc.drawImage(createCircledNumber("Sri"), 70, 105);
+    
+    VBox vBox = new VBox();
+    vBox.getChildren().addAll(canvas);
+    
+    BorderPane pane = new BorderPane();
+    pane.setCenter(canvas);
+    
+    Scene newScene = new Scene(pane, 400, 400);
+    newScene.getStylesheets().add(getClass().getResource("Scene.css").toExternalForm());
+    
+    stage.setScene(newScene);
+    stage.show();
+    
+  }
 
   /**
    * @param args
